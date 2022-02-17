bdc9c6e065a2b9fb931c570c353319ab5dac7031
Tilok Paul <tilokpaul.web@gmail.com>
2022-02-17 22:16:23 +0600

    Uddate validation task and ready to deploy

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// Massage error element 
 let savingError = document.getElementById('saving-error-massage')
 let balanceError = document.getElementById('blance-error-massage')
 // Input value collect
@@ -6,13 +7,15 @@ function getInputValue(idName){
     const inputValue = parseFloat(inputField.value)
 
     if(typeof(inputValue) == 'number' && inputValue >= 0){
+        const errorContents = document.querySelectorAll('.input-error')
+        for(error of errorContents){
+            error.remove();
+        }
         return inputValue
     }else if(inputValue < 0){
         inputField.insertAdjacentHTML("afterend", '<p class="input-error text-danger">Negative number</p>');
-        return 0
     }else{
         inputField.insertAdjacentHTML("afterend", '<p class="input-error text-danger">Invalid input!! String</p>');
-        return 0
     }
 
 }
@@ -25,8 +28,16 @@ function expenceBalanceCalculation(){
     const clothExpence = getInputValue('cloth-expence')
     const totalExpence = foodExpence + rentExpence + clothExpence
     const balance = incomeAmount - totalExpence;
-    document.getElementById('total-expence').innerText = totalExpence
-    document.getElementById('balance').innerText = balance
+
+    if(incomeAmount >=  totalExpence){
+        document.getElementById('total-expence').innerText = totalExpence
+        document.getElementById('balance').innerText = balance
+        balanceError.innerText =''
+    }else if(incomeAmount == undefined || foodExpence == undefined || rentExpence == undefined || clothExpence == undefined){
+        balanceError.innerText ='Set the Value of INPUT field or Input "0"'
+    }else{
+        balanceError.innerText = "Total Expenses is more than Income"
+    }
 }
 // Saving calculation 
 function savingCalculation(){
@@ -46,11 +57,12 @@ function savingCalculation(){
         balanceError.innerText = savingsAmountGet.innerText = remainingGet.innerText = ""   
     }
 }
-
+// calculation event handaler
 document.getElementById('calculate-btn').addEventListener('click', function(){
     expenceBalanceCalculation()
     
 })
+// Savings event handaler
 document.getElementById('saving-btn').addEventListener('click', function(){
     savingCalculation()
 
